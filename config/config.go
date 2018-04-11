f5e0e9173158df1010b662a7e54de24a25e8a55c
David Sullivan <dsullivan0190@gmail.com>
2018-04-11 13:44:36 +0100

    Reordered config to keep similar vars together

--- config/config.go
+++ config/config.go
@@ -4,12 +4,12 @@ var SQSAnalyticsURL = ""
 
 var SQSWaitTimeout int64 = 20
 
+var MaxSQSMessages int64 = 10
+
 var MongoDBUrl = "localhost:27017"
 
 var MongoDBDatabase = "local"
 
 var MongoDBCollection = "searchstats"
-
-var MaxSQSMessages int64 = 10
 
 var RunAllOnStartup = true
