6c2765b603f8dd46e43e757abdcc0a91056cd968
David Sullivan <dsullivan0190@gmail.com>
2018-04-13 12:53:53 +0100

    Added assertions

--- analytics/backend_sqs_test.go
+++ analytics/backend_sqs_test.go
@@ -19,7 +19,7 @@ func (m mockedReceiveMsgs) ReceiveMessageRequest(in *sqs.ReceiveMessageInput) sq
   output := sqs.ReceiveMessageOutput{
     Messages: []sqs.Message{
       {
-        Body: aws.String(`{"created":"1","url":"test_url","term":"test","listType":"test","gaID":"testgaID","gID":"testgID","pageIndex":0,"linkIndex":0,"pageSize":0}`),
+        Body: aws.String(`{"created":"Now","url":"/test/url","term":"test_term","listType":"test_list_type","gaID":"testgaID","gID":"testgID","pageIndex":0,"linkIndex":1,"pageSize":2}`),
         ReceiptHandle: aws.String("testHandle"),
       },
     },
@@ -40,11 +40,22 @@ func TestSQSReaderImpl_GetMessages(t *testing.T) {
   }
 
   messages, err := q.GetMessages(20, 10)
+  message := messages[0]
 
   Convey("Given valid input parameters", t, func() {
     So(q, ShouldNotBeNil)
     So(messages, ShouldNotBeNil)
     So(err, ShouldBeNil)
     So(len(messages), ShouldEqual, 1)
+
+    So(message.Created, ShouldEqual, "Now")
+    So(message.Url, ShouldEqual, "/test/url")
+    So(message.Term, ShouldEqual, "test_term")
+    So(message.ListType, ShouldEqual, "test_list_type")
+    So(message.GaID, ShouldEqual, "testgaID")
+    So(message.GID, ShouldEqual, "testgID")
+    So(message.PageIndex, ShouldEqual, 0)
+    So(message.LinkIndex, ShouldEqual, 1)
+    So(message.PageSize, ShouldEqual, 2)
   })
 }
